369ecebc0affc57c07f2c9b67421196ad460aa9d
b-hyoung <youqlrqod@gmail.com>
2025-09-19 22:11:39 +0900

    수정: 폴더이름

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,7 +2,7 @@ import localFont from 'next/font/local'
 
 // JetBrains Mono 폰트를 불러오면서 CSS 변수 이름을 지정해줍니다.
 export const jetbrains_mono = localFont({
-  src: "./fonts/jetbrains_Mono/webfonts/JetBrainsMono-Regular.woff2",
+  src: "./fonts/jetBrains_Mono/webfonts/JetBrainsMono-Regular.woff2",
   display: 'swap',
   // 이 부분이 핵심입니다!
   variable: '--font-jetbrains-mono', 
